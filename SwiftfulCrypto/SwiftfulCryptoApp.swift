2f6926d4c760e9d7237c293f4162d7dbfc94dce0
Tony <tong.decula@gmail.com>
2023-03-13 22:04:05 -0400

    feat: first init.

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Tony on 2023-03-13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
